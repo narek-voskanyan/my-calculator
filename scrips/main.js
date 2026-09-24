af29e51967e1017cc24a07fbbdd388148676c180
narek-voskanyan <narek.voskanyan91@gmail.com>
2026-09-24 16:51:09 +0400

    Refactor calculator logic using class and event listeners

--- scrips/main.js
+++ scrips/main.js
@@ -1,101 +1,90 @@
-var firstNumber = document.getElementById('input-first-number-field');
-var secondNumber = document.getElementById('input-second-number-field');
-
-var calculateButton = document.getElementById('calculate-button');
-
-var resultField = document.getElementById('result');
-
-var clearButton = document.getElementById('clear-button');
-
-var multipleButton = document.getElementById('operation-multiply');
-var divideButton = document.getElementById('operation-divide');
-var addButton = document.getElementById('operation-add');
-var subtractButton = document.getElementById('operation-subtract');
-
-
-
-function zeroDivision() {
-
-    if (
-        (Number(secondNumber.value) === 0) &&
-        divideButton.checked
-    ) {
-        resultField.textContent = 'Error';
-       return true
-    }else{
-
-        return false;
-    }
-}
-
-function checkEmptyFields() {
-    if(firstNumber.value === '' || secondNumber.value === '') {
-        resultField.textContent = 'Fill in all fields before calculating';
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function isOperationSelected (){
-    const selectedOperation = document.querySelector('input[name="operation"]:checked') !== null;
-    if(!selectedOperation) {
-        resultField.textContent = 'Please select an operation';
-        return true;
-    }else{
-        return false;
-    }
-}
-
-
-/* Calculate button */
-calculateButton.onclick = function() {
-
-    let firstNumberValue = Number(firstNumber.value);
-    let secondNumberValue = Number(secondNumber.value);
-
-    let isEmptyFields = checkEmptyFields();
-    if(isEmptyFields) {
-     return;
+class Calculator {
+    constructor(){
+        this.firstNumber = document.getElementById('input-first-number-field');
+        this.secondNumber = document.getElementById('input-second-number-field');
+        this.calculateButton = document.getElementById('calculate-button');
+        this.resultField = document.getElementById('result');
+        this.clearButton = document.getElementById('clear-button');
+        this.multipleButton = document.getElementById('operation-multiply');
+        this.divideButton = document.getElementById('operation-divide');
+        this.addButton = document.getElementById('operation-add');
+        this.subtractButton = document.getElementById('operation-subtract');
     }
 
-    let isOperationSele = isOperationSelected();
-    if(isOperationSele) {
-        return;
+    checkEmptyFields() {
+        if(this.firstNumber.value === '' || this.secondNumber.value === '') {
+            this.resultField.textContent = 'Fill in all fields before calculating';
+            return true;
+        }else{
+            return false;
+        }
     }
-
-   let isZeroDivision = zeroDivision();
-   if(isZeroDivision) {
-    return;
-   }
-
+    isOperationSelected() {
+        const selectedOperation = document.querySelector('input[name="operation"]:checked') !== null;
+        if(!selectedOperation) {
+            this.resultField.textContent = 'Please select an operation';
+            return true;
+        }else{
+            return false;
+        }
+    }
     
-    let result = 0;
-    if(multipleButton.checked) {
-         result = firstNumberValue * secondNumberValue;
-    } else if(divideButton.checked) {
-         result = firstNumberValue / secondNumberValue;
-    } else if(addButton.checked) {
-         result = firstNumberValue + secondNumberValue;
-    } else if(subtractButton.checked) {
-         result = firstNumberValue - secondNumberValue;
+/* Calculate button */
+    calculate() {
+        let firstNumberValue = Number(this.firstNumber.value);
+        let secondNumberValue = Number(this.secondNumber.value);
+        let isEmptyFields = this.checkEmptyFields();
+        if(isEmptyFields) {
+            return;
+        }
+        let isOperationSelected = this.isOperationSelected();
+        if(isOperationSelected) {
+            return;
+        }
+        let isZeroDivision = this.zeroDivision();
+        if(isZeroDivision) {
+            return;
+        }
+        let result = 0;
+        if(this.multipleButton.checked) {
+            result = firstNumberValue * secondNumberValue;
+        } else if(this.divideButton.checked) {
+            result = firstNumberValue / secondNumberValue;
+        } else if(this.addButton.checked) {
+            result = firstNumberValue + secondNumberValue;
+        } else if(this.subtractButton.checked) {
+            result = firstNumberValue - secondNumberValue;
+        }
+    
+
+    this.resultField.textContent = result;
+}
+
+    /* Clear button */
+    clear() {
+        this.resultField.textContent = '—';
+        this.firstNumber.value = "";
+        this.secondNumber.value = "";
     }
-    resultField.textContent = result;
 
-}
-
-/* Clear button */
-clearButton.onclick = function() {
-    resultField.textContent = '—';
-    firstNumber.value = " ";
-    secondNumber.value = " ";
-}
-
-/* verify 0 in division*/
-
-firstNumber.oninput = function() {
-    if(secondNumber.value === '0' && divideButton.checked) {
-        resultField.textContent = 'Error';
-        
+    zeroDivision() {
+        if(this.secondNumber.value === '0' && this.divideButton.checked) {
+            this.resultField.textContent = 'Error';
+            return true;
+        }else{
+            return false;
+        }
     }
 }
+
+
+const calculator = new Calculator();
+
+calculator.calculateButton.addEventListener("click", function () {
+    calculator.calculate();
+});
+
+calculator.clearButton.addEventListener('click', function () {
+    calculator.clear();
+});
+
